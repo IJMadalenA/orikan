ef91b3f4714e80c64b1a77f08686238980952cfb
IJMadalenA <developmentim@qaleon.com>
2023-06-04 15:14:09 +0200

    update poetry.

--- orikan/asgi.py
+++ orikan/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for Monitorum project.
+ASGI config for orikan project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Monitorum.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'orikan.settings')
 
 application = get_asgi_application()
