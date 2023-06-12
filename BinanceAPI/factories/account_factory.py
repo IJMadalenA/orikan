8acfae2f68ff2eb264f2953c86f0aaab73c62c64
IJMadalenA <developmentim@qaleon.com>
2023-06-12 13:58:55 +0200

    update models.

--- BinanceAPI/factories/account_factory.py
+++ BinanceAPI/factories/account_factory.py
@@ -1,6 +1,6 @@
 from factory.fuzzy import (
     FuzzyDecimal,
-    FuzzyChoice,
+    FuzzyChoice, FuzzyInteger,
 )
 from factory import LazyAttribute
 
@@ -9,13 +9,63 @@ from BinanceAPI.models import Account
 
 
 class AccountFactory(BaseBinanceFactory):
-    account_id = LazyAttribute(lambda obj: BaseBinanceFactory.generate_unique_id())
     status = FuzzyChoice((
         ('ACTIVE', 'Active'),
         ('INACTIVE', 'Inactive'),
-    ))
-    balance = FuzzyDecimal(low=1.000).fuzz()
-    available_balance = FuzzyDecimal(low=1.000).fuzz()
+    )).fuzz()
+    maker_commission = FuzzyInteger(low=1000).fuzz()
+    taker_commission = FuzzyInteger(low=1000).fuzz()
+    buyer_commission = FuzzyInteger(low=1000).fuzz()
+    seller_commission = FuzzyInteger(low=1000).fuzz()
+    can_trade = FuzzyChoice([
+        True,
+        False,
+    ]).fuzz()
+    can_withdraw = FuzzyChoice([
+        True,
+        False,
+    ]).fuzz()
+    can_deposit = FuzzyChoice([
+        True,
+        False,
+    ]).fuzz()
+    ip_restrict = FuzzyChoice([
+        True,
+        False,
+    ]).fuzz()
+    enable_withdrawals = FuzzyChoice([
+        True,
+        False,
+    ]).fuzz()
+    enable_internal_transfer = FuzzyChoice([
+        True,
+        False,
+    ]).fuzz()
+    permits_universal_transfer = FuzzyChoice([
+        True,
+        False,
+    ]).fuzz()
+    enable_vanilla_options = FuzzyChoice([
+        True,
+        False,
+    ]).fuzz()
+    enable_reading = FuzzyChoice([
+        True,
+        False,
+    ]).fuzz()
+    enable_futures = FuzzyChoice([
+        True,
+        False,
+    ]).fuzz()
+    enable_margin = FuzzyChoice([
+        True,
+        False,
+    ]).fuzz()
+    enable_spot_and_margin_trade = FuzzyChoice([
+        True,
+        False,
+    ]).fuzz()
+    trading_authority_expiration_time = FuzzyInteger(low=1000).fuzz()
 
     class Meta:
         model = Account
--- BinanceAPI/factories/balance_factory.py
+++ BinanceAPI/factories/balance_factory.py
@@ -1,10 +1,12 @@
+from factory import SubFactory
 from factory.fuzzy import (
     FuzzyDecimal,
     FuzzyChoice,
 )
 
+from BinanceAPI.factories import AccountFactory
 from BinanceAPI.factories.base_binance_factory import BaseBinanceFactory
-from BinanceAPI.models.balance_model import Balance
+from BinanceAPI.models.balance_spot_model import BalanceSpot
 
 
 class BalanceFactory(BaseBinanceFactory):
@@ -12,12 +14,11 @@ class BalanceFactory(BaseBinanceFactory):
         ('BTC', 'Bitcoin'),
         ('ETH', 'Ethereum'),
     )).fuzz()
+    account = SubFactory(AccountFactory)
     free = FuzzyDecimal(low=1.000).fuzz()
     locked = FuzzyDecimal(low=1.000).fuzz()
     total = FuzzyDecimal(low=1.000).fuzz()
     in_order = FuzzyDecimal(low=1.000).fuzz()
-    btc_value = FuzzyDecimal(low=1.000).fuzz()
-    eth_value = FuzzyDecimal(low=1.000).fuzz()
 
     class Meta:
-        model = Balance
+        model = BalanceSpot
