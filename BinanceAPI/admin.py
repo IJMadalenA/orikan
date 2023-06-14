4b32ea053510171bff96f43e24696dfed19250cb
IJMadalenA <developmentim@qaleon.com>
2023-06-14 12:49:39 +0200

    update admin.

--- BinanceAPI/admin.py
+++ BinanceAPI/admin.py
@@ -3,8 +3,10 @@ from django.contrib.admin import ModelAdmin
 
 from BinanceAPI.models import Account
 
-Account.load_account_data()
-# Register your models here.
+# Al momento de inicializar el Admin, si no hay un usuario registrado, esta función
+# se ejecutará y cargará el usuario relacionado con las llaves.
+if not Account.objects.first():
+    Account.load_account_data()
 
 
 @admin.register(Account)
@@ -12,14 +14,32 @@ class AccountAdmin(ModelAdmin):
     # Campos a mostrar en la lista
     list_display = [
         'id',
+        'name',
         'status',
-        'maker_commission',
-        'taker_commission'
+        'ip_restrict',
+        'can_trade',
     ]
     # Campos de solo lectura
     readonly_fields = [
         'id',
-        'status'
+        'status',
+        'maker_commission',
+        'taker_commission',
+        'buyer_commission',
+        'seller_commission',
+        'can_trade',
+        'can_withdraw',
+        'can_deposit',
+        'ip_restrict',
+        'enable_withdrawals',
+        'enable_internal_transfer',
+        'permits_universal_transfer',
+        'enable_vanilla_options',
+        'enable_reading',
+        'enable_futures',
+        'enable_margin',
+        'enable_spot_and_margin_trade',
+        'trading_authority_expiration_time',
     ]
     # Acción personalizada
     actions = ['load_account_data']
