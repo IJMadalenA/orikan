19ffd3250f13554d82ee7fdc566650d04793d4df
IJMadalenA <developmentim@qaleon.com>
2023-06-14 11:38:16 +0200

    update admin.

--- BinanceAPI/admin.py
+++ BinanceAPI/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
+from BinanceAPI.models import Account
+
+Account.load_account_data()
 # Register your models here.
+
+
+@admin.register(Account)
+class AccountAdmin(ModelAdmin):
+    # Campos a mostrar en la lista
+    list_display = [
+        'id',
+        'status',
+        'maker_commission',
+        'taker_commission'
+    ]
+    # Campos de solo lectura
+    readonly_fields = [
+        'id',
+        'status'
+    ]
+    # Acción personalizada
+    actions = ['load_account_data']
+
+    @admin.action(description="Cargar datos en la cuenta.")
+    def load_account_data(self, request, queryset):
+        # Lógica para ejecutar el método de actualización
+        for account in queryset:
+            account.load_account_data()
+
+    load_account_data.short_description = "Cargar datos de la cuenta"
