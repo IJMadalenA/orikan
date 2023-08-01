55df0b1c9b98c5868433dd61fa7bc79b7608f1d0
IJMAdalenA <IJMadalenA@outlook.com>
2023-08-01 16:23:43 +0200

    update symbol factory.

--- BinanceAPI/factories/symbol_factory.py
+++ BinanceAPI/factories/symbol_factory.py
@@ -16,11 +16,20 @@ class SymbolFactory(BaseBinanceFactory):
     symbol = LazyAttribute(lambda obj: BaseBinanceFactory.generate_unique_id())
     status = FuzzyText(length=10)
     base_asset = SubFactory(AssetFactory)
+    quote_percent_precision = FuzzyInteger(0, 10)
+    quote_asset_precision = FuzzyInteger(0, 10)
+    base_commission_precision = FuzzyInteger(0, 10)
+    quote_commission_precision = FuzzyInteger(0, 10)
     base_asset_precision = FuzzyInteger(0, 10)
     quote_asset = SubFactory(AssetFactory)
-    quote_asset_precision = FuzzyInteger(0, 10)
     order_types = FuzzyText(length=10)
     iceberg_allowed = FuzzyChoice([True, False])
+    oco_allowed = FuzzyChoice([True, False])
+    quote_order_qty_market_allowed = FuzzyChoice([True, False])
+    allow_trailing_stop = FuzzyChoice([True, False])
+    cancel_replace_allowed = FuzzyChoice([True, False])
+    is_spot_trading_allowed = FuzzyChoice([True, False])
+    is_margin_trading_allowed = FuzzyChoice([True, False])
 
     class Meta:
         model = Symbol
