c246b038ca681d679b584c3138ec20ed755c4c46
IJMAdalenA <IJMadalenA@outlook.com>
2023-08-01 16:23:25 +0200

    update.

--- BinanceAPI/tests/models_tests/avg_model_test.py
+++
@@ -1,8 +0,0 @@
-from BinanceAPI.models.avg_price_model import AvgPrice as ModelImported
-from BinanceAPI.factories.avg_price_factory import AvgPriceFactory as FactoryImported
-from BinanceAPI.tests.models_tests.base_binance_model_test import BaseModelTestCase
-
-
-class AvgPriceModelTestCase(BaseModelTestCase):
-    model = ModelImported
-    factory = FactoryImported
