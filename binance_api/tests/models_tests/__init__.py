2092c85e7de9606d25e1d4e3e5e1c05eb9779345
IJMAdalenA <IJMadalenA@outlook.com>
2023-08-22 16:13:16 +0200

    update model tests init.

--- binance_api/tests/models_tests/__init__.py
+++ binance_api/tests/models_tests/__init__.py
@@ -3,6 +3,7 @@ from binance_api.tests.models_tests.asset_model_test import AssetModelTestCase
 from binance_api.tests.models_tests.balance_spot_model_test import BalanceSpotModelTestCase
 from binance_api.tests.models_tests.deposit_model_test import DepositModelTestCase
 from binance_api.tests.models_tests.network_model_test import NetworkModelTestCase
+from binance_api.tests.models_tests.order_model_test import OrderModelTestCase
 from binance_api.tests.models_tests.order_book_model_test import OrderBookModelTestCase
 from binance_api.tests.models_tests.symbol_model_test import SymbolModelTestCase
 from binance_api.tests.models_tests.ticker_model_test import TickerModelTestCase
