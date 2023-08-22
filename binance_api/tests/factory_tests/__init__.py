a588828525fe7c3d80efdd0a9920dbd19bb3054f
IJMAdalenA <IJMadalenA@outlook.com>
2023-08-22 16:12:57 +0200

    update factory init.

--- binance_api/tests/factory_tests/__init__.py
+++ binance_api/tests/factory_tests/__init__.py
@@ -18,6 +18,7 @@ from binance_api.tests.factory_tests.filters_factory_test import (
 )
 from binance_api.tests.factory_tests.network_factory_test import NetworkFactoryTestCase
 from binance_api.tests.factory_tests.order_factory_test import OrderFactoryTestCase
+from binance_api.tests.factory_tests.order_book_factory_test import OrderBookFactoryTestCase
 from binance_api.tests.factory_tests.asset_factory_test import AssetFactoryTestCase
 from binance_api.tests.factory_tests.ticker_factory_test import TickerFactoryTestCase
 from binance_api.tests.factory_tests.trade_factory_test import TradeFactoryTestCase
